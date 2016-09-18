554557209409fee32a19a3579414a402ec6ca7ee
Cassidy Marble <marblenix@users.noreply.github.com>
2016-09-18 12:23:30 -0500

    Make neovim install script use the correct directories

--- bin/install-nvim.sh
+++ bin/install-nvim.sh
@@ -8,12 +8,12 @@ main() {
 }
 
 install() {
-  git clone https://github.com/marblenix/vim-setup ~/.vim
-  mkdir -p ~/.vim/bundle ~/.vim/session/{backup,swap,undo}
-  git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-  nvim -U ~/.vim/plugin.vim -u ~/.vim/plugin.vim +PluginInstall +qall &>/dev/null
-  mkdir -p ~/.config/nvim
-  ln -s ~/.vim/vimrc ~/.config/nvim/init.vim
+  git clone -b neovim https://github.com/marblenix/vim-setup "$HOME/.config/nvim"
+  mkdir -p "$HOME/.config/nvim/bundle" "$HOME/.config/nvim/session/"{backup,swap,undo}
+  git clone https://github.com/gmarik/Vundle.vim.git "$HOME/.config/nvim/bundle/Vundle.vim"
+  nvim -U "$HOME/.config/nvim/plugin.vim" -u "$HOME/.config/nvim/plugin.vim" +PluginInstall +qall &>/dev/null
+  mkdir -p "$HOME/.config/nvim"
+  ln -s "$HOME/.config/nvim/vimrc" "$HOME/.config/nvim/init.vim"
 }
 
 main

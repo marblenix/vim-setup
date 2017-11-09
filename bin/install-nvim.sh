310e80df179963fd93c4f460f74f9281528b4f75
Cassidy Marble <marblenix@users.noreply.github.com>
2017-11-08 21:45:51 -0600

    Fix installer

--- bin/install-nvim.sh
+++ bin/install-nvim.sh
@@ -3,16 +3,16 @@
 set -eu
 
 main() {
-  install
-  echo 'Setup complete.'
+    install
+    echo 'Setup complete.'
 }
 
 install() {
-  mkdir -p "$HOME/.config"
-  git clone -b neovim https://github.com/marblenix/vim-setup "$HOME/.config/nvim"
-  mkdir -p "$HOME/.cache/nvim/pack/minpac/opt" "$HOME/.cache/nvim/session/"{backup,swap,undo}
-  git clone https://github.com/k-takata/minpac.git "$HOME/.cache/nvim/pack/minpac/opt/minpac"
-  nvim -U "$HOME/.config/nvim/plugin.vim" -u "$HOME/.config/nvim/plugin.vim" +PluginInstall +qall
+    mkdir -p "$HOME/.config"
+    git clone -b neovim https://github.com/marblenix/vim-setup "$HOME/.config/nvim"
+    mkdir -p "$HOME/.config/nvim/pack/minpac/opt" "$HOME/.cache/nvim/session/"{backup,swap,undo}
+    git clone https://github.com/k-takata/minpac.git "$HOME/.config/nvim/pack/minpac/opt/minpac"
+    nvim -U "$HOME/.config/nvim/plugin.vim" -u "$HOME/.config/nvim/plugin.vim" +PluginInstall +qall
 }
 
 main

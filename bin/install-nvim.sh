47f2d32631ae98d3aada5bfd94e2d9e7def645b8
Caleb Marble <marblenix@users.noreply.github.com>
2017-11-08 21:59:09 -0600

    Update installer script to work with minpac

--- bin/install-nvim.sh
+++ bin/install-nvim.sh
@@ -12,7 +12,7 @@ install() {
     git clone -b neovim https://github.com/marblenix/vim-setup "$HOME/.config/nvim"
     mkdir -p "$HOME/.config/nvim/pack/minpac/opt" "$HOME/.cache/nvim/session/"{backup,swap,undo}
     git clone https://github.com/k-takata/minpac.git "$HOME/.config/nvim/pack/minpac/opt/minpac"
-    nvim -U "$HOME/.config/nvim/plugin.vim" -u "$HOME/.config/nvim/plugin.vim" +PluginInstall +qall
+    nvim -U "$HOME/.config/nvim/plugin.vim" -u "$HOME/.config/nvim/plugin.vim" +PluginInstallAndQuit &>/dev/null
 }
 
 main
